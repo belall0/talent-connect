da3bdf0e358724dfff7f4c7d1a9d1ac95379ea47
belal muhammad <belallmuhammad0@gmail.com>
2025-01-17 09:22:22 +0200

    feat: Refactor JobPage to use loader for job data fetching;

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,19 @@ import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobsPage from './pages/JobsPage';
-import JobPage from './pages/JobPage';
-
-// define routes
-const routes = createRoutesFromElements(
-  <Route path="/" element={<MainLayout />}>
-    <Route index element={<HomePage />} />
-    <Route path="/jobs" element={<JobsPage />} />
-    <Route path="/jobs/:id" element={<JobPage />} />
-    <Route path="*" element={<NotFoundPage />} />
-  </Route>,
-);
-const router = createBrowserRouter(routes);
+import JobPage, { jobLoader } from './pages/JobPage';
 
 const App = () => {
+  // define routes
+  const routes = createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>,
+  );
+  const router = createBrowserRouter(routes);
   return <RouterProvider router={router} />;
 };
 
